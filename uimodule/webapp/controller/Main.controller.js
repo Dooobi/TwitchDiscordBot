02e4ddbc58f2a900354d5b7d6249b40ce3cfb85a
Demindh <tobias.stelzer93@gmx.de>
2021-01-04 16:04:59 +0100

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/Main.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "de/demindh/TwitchDiscordBot/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("de.demindh.TwitchDiscordBot.controller.Main", {});
+});
